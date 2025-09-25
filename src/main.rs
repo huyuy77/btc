b2f0a65bb05ed73f6edfe6713ac82d070f55c065
arielherself <arielherself@duck.com>
2025-09-25 17:43:08 +0800

    feat: home page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let index = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file("www/static/index.html"));
+
+    warp::serve(index).run(([0, 0, 0, 0], 3000)).await;
 }
